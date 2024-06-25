2b13dca1455886baf7571e990e6680d2fa96d5c9
David-Pintado <david02pg@gmail.com>
2024-06-25 17:42:46 +0200

    UPDATE setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pythonLib',
     packages=find_packages(include=['pythonLib']),
     version='0.1.0',
-    description='Libreria de Python del primer caso de ejemplo',
+    description='Libreria de Python del primer caso piloto',
     author='David Pintado Gómez',
     install_requires=[],
     setup_requires=['pytest-runner'],
